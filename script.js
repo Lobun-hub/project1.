85a75abfee51e367776831dca3fb3a7659dfa1bc
Francis Lobun <francislotaba@gmail.com>
2025-11-03 01:43:48 +0300

    added responsiveness

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-  const toggleBtn = document.querySelector(".menu-toggle");
-  const navLinks = document.querySelector("header nav ul");
-
-  toggleBtn.addEventListener("click", () => {
-    navLinks.classList.toggle("show");
-  });
